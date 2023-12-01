c7587b8f6283fd78b276b4da55aa360b7e3d5c33
Stephen Bain <stephenbain@iheartmedia.com>
2023-12-01 12:14:27 -0600

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,38 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun String.sumAfterReplace(): Int {
+        return first { it.isDigit() }.digitToInt() * 10 + last { it.isDigit() }.digitToInt()
     }
+
+    fun part1(input: List<String>): Int {
+        return input.sumOf {  line ->
+            line.sumAfterReplace()
+        }
+    }
+
+    val wordMap = mapOf(
+        "one" to "o1e",
+        "two" to "t2o",
+        "three" to "t3e",
+        "four" to "4",
+        "five" to "5e",
+        "six" to "6",
+        "seven" to "7n",
+        "eight" to "e8t",
+        "nine" to "9e"
+    )
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf { lineIn ->
+            var line = lineIn
+            wordMap.forEach { (key, value) ->
+                line = line.replace(key, value)
+            }
+            line.sumAfterReplace()
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    "part 1: ${part1(input)}".println()
+    "part 2: ${part2(input)}".println()
 }
