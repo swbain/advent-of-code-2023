5dfe1c319e5bdaac3d7bb2a43d42b8ff5830244b
Stephen Bain <swbain@gmail.com>
2023-12-02 01:12:16 -0600

    day 2 part one

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,6 +1,27 @@
 fun main() {
+
+    fun String.isColorValid(): Boolean = split(" ").run {
+        val blockCount = first().toInt()
+        when (last()) {
+            "red" -> blockCount <= 12
+            "green" -> blockCount <= 13
+            "blue" -> blockCount <= 14
+            else -> false // illegal
+        }
+    }
+
+    fun String.isSetValid(): Boolean = split(", ").all { it.isColorValid() }
+
+    fun String.areAllSetsValid(): Boolean = split("; ").all { it.isSetValid() }
+
+    fun String.idValue(): Int {
+        // return game id if all sets pulled are valid, otherwise return zero
+        val split = removePrefix("Game ").split(": ")
+        return if (split.last().areAllSetsValid()) split.first().toInt() else 0
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { it.idValue() }
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +30,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 8)
 
     val input = readInput("Day02")
     part1(input).println()
