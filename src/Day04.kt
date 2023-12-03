493e8c725d25e83130192b56f646f7af623f19c9
Stephen Bain <stephenbain@iheartmedia.com>
2023-12-03 16:01:34 -0600

    day 4 setup

--- src/Day04.kt
+++ src/Day04.kt
@@ -9,9 +9,18 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day04")
-    part1(input).println()
-    part2(input).println()
+    printOutput(
+        day = 4,
+        part1 = Results(
+            expectedTestResult = 0,
+            testResult = runAndMeasure { part1(testInput) },
+            actualResult = runAndMeasure { part1(input) }
+        ),
+        part2 = Results(
+            expectedTestResult = 0,
+            testResult = runAndMeasure { part2(testInput) },
+            actualResult = runAndMeasure { part2(input) }
+        ),
+    )
 }
