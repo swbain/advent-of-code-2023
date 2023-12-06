7a5f9d105168f8141f8f1380654541e11516b58d
Stephen Bain <stephenbain@iheartmedia.com>
2023-12-05 23:54:07 -0600

    day 6 part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -3,26 +3,31 @@ import kotlin.time.measureTimedValue
 fun main() {
     data class Race(val time: Long, val recordDistance: Long)
 
-    fun Race.isWin(holdTime: Long): Boolean {
-        val distance = holdTime * (time - holdTime)
-        return distance > recordDistance
+    fun Race.isWin(holdTime: Long): Boolean = holdTime * (time - holdTime) > recordDistance
+
+    fun Race.winCount(): Int = (1..<time).count(this::isWin)
+
+    fun String.trimmedToValues(): List<String> {
+        return split(":").last().split(" ").filter { it.isNotBlank() }
     }
 
-    fun Race.winCount(): Int = (1..<time).count { isWin(it) }
+    fun String.raceValues(): List<Long> = trimmedToValues().map { it.toLong() }
 
-    fun String.raceValues(): List<Long> = split(":").last()
-        .split(" ")
-        .filter { it.isNotBlank() }
-        .map { it.toLong() }
+    fun String.combinedNumbers(): Long = trimmedToValues().reduce { acc, s -> acc + s }.toLong()
 
     fun List<String>.races(): List<Race> = first().raceValues().zip(last().raceValues(), ::Race)
+
+    fun List<String>.partTwoRace(): Race = Race(
+        time = first().combinedNumbers(),
+        recordDistance = last().combinedNumbers(),
+    )
 
     fun part1(input: List<String>): Long {
         return input.races().map { it.winCount().toLong() }.reduce(Long::times)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.partTwoRace().apply { println() }.winCount()
     }
 
     val testInput = readInput("Day06_test")
@@ -35,7 +40,7 @@ fun main() {
             actualResult = measureTimedValue { part1(input) }
         ),
         part2 = Results(
-            expectedTestResult = 0,
+            expectedTestResult = 71503,
             testResult = measureTimedValue { part2(testInput) },
             actualResult = measureTimedValue { part2(input) }
         ),
