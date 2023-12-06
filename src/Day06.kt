7670647af9b0371dc6ef756598348da75333cb2e
Stephen Bain <stephenbain@iheartmedia.com>
2023-12-05 23:02:15 -0600

    day 6 setup

---
+++ src/Day06.kt
@@ -0,0 +1,27 @@
+import kotlin.time.measureTimedValue
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    val testInput = readInput("Day06_test")
+    val input = readInput("Day06")
+    printOutput(
+        day = 5,
+        part1 = Results(
+            expectedTestResult = 0,
+            testResult = measureTimedValue { part1(testInput) },
+            actualResult = measureTimedValue { part1(input) }
+        ),
+        part2 = Results(
+            expectedTestResult = 0,
+            testResult = measureTimedValue { part2(testInput) },
+            actualResult = measureTimedValue { part2(input) }
+        ),
+    )
+}
