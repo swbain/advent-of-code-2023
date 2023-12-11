3bde596e5668a372bff74d77ed022416b949ee93
Stephen Bain <swbain@gmail.com>
2023-12-11 14:07:15 -0600

    day 11 setup

---
+++ src/Day11.kt
@@ -0,0 +1,27 @@
+import kotlin.time.measureTimedValue
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    val testInput = readInput("Day11_test")
+    val input = readInput("Day11")
+    printOutput(
+        day = 11,
+        part1 = Results(
+            expectedTestResult = 0,
+            testResult = measureTimedValue { part1(testInput) },
+            actualResult = measureTimedValue { part1(input) }
+        ),
+        part2 = Results(
+            expectedTestResult = 0,
+            testResult = measureTimedValue { part2(testInput) },
+            actualResult = measureTimedValue { part2(input) }
+        ),
+    )
+}
