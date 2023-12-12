289f5bf36b6bb201f57f75eb72b9d8825d8bf150
Stephen Bain <swbain@gmail.com>
2023-12-12 01:36:02 -0600

    🧹

--- src/Day11.kt
+++ src/Day11.kt
@@ -15,41 +15,40 @@ fun main() {
 
     fun List<Point>.emptyColumns(): List<Int> = (0..maxOf { it.x }).filter { x -> !any { it.x == x } }
 
-    fun List<Point>.sumOfLengths(): Int {
-
+    fun List<Point>.sumOfLengths(multiplier: Long): Long {
         val emptyRows = emptyRows()
         val emptyColumns = emptyColumns()
 
-        fun Point.shortestPathTo(point: Point): Int {
-            val xDiff = abs(x - point.x) + emptyColumns.count { (min(x, point.x)..max(x, point.x)).contains(it) }
-            val yDiff = abs(y - point.y) + emptyRows.count { (min(y, point.y)..max(y, point.y)).contains(it) }
+        fun Point.shortestPathTo(point: Point): Long {
+            val extraX = emptyColumns.count { (min(x, point.x)..max(x, point.x)).contains(it) } * (multiplier - 1)
+            val xDiff = abs(x - point.x) + extraX
+            val extraY = emptyRows.count { (min(y, point.y)..max(y, point.y)).contains(it) } * (multiplier - 1)
+            val yDiff = abs(y - point.y).toLong() + extraY
             return xDiff + yDiff
         }
 
-        fun IndexedValue<Point>.shortestPaths(): Int = takeLast(size - index).sumOf { value.shortestPathTo(it) }
+        fun IndexedValue<Point>.shortestPaths(): Long = takeLast(size - index).sumOf { value.shortestPathTo(it) }
 
         return withIndex().sumOf { it.shortestPaths() }
     }
 
-    fun part1(input: List<String>): Int = input.getGalaxies().sumOfLengths()
+    fun part1(input: List<String>): Long = input.getGalaxies().sumOfLengths(2)
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>): Long = input.getGalaxies().sumOfLengths(1000000)
 
     val testInput = readInput("Day11_test")
     val input = readInput("Day11")
     printOutput(
         day = 11,
         part1 = Results(
-            expectedTestResult = 374,
+            expectedTestResult = 374L,
             testResult = measureTimedValue { part1(testInput) },
             actualResult = measureTimedValue { part1(input) }
         ),
         part2 = Results(
-            expectedTestResult = 0,
-            testResult = measureTimedValue { 0 },
-            actualResult = measureTimedValue { 0 }
+            expectedTestResult = 8410L,
+            testResult = measureTimedValue { part2(testInput) },
+            actualResult = measureTimedValue { part2(input) }
         ),
     )
 }
